90e5c81ce4f6176f46f73637b732445ff1e017ad
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-06 16:48:46 +0900

    Extract search API hooks out of SearchScreen
    Originally, I implemented component with class based.
    But when I tried to extract searchApi logic, I could not manage it.
    So I gave up class style and swiched to function style component.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,54 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp"
+import useResults from "../hooks/useResults";
 
-class SearchScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: "",
-      results:[],
-      errorMessage: ""
-    };
-  }
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
-  componentDidMount() {
-    this.searchApi(this.state.term);
-  }
-
-  searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search',
-        {
-          params: {
-            limit: 50,
-            term: searchTerm,
-            location: 'san jose'
-          }
-        });
-      this.setState({results: response.data.businesses});
-    } catch (err) {
-      this.setState({errorMessage: 'Something went wrong'});
-    }
-  };
-
-  render() {
-    return (
-      <View style={styles.background}>
-        <SearchBar
-          term={this.state.term}
-          onTermChange={(newTerm) => {
-            this.setState({term: newTerm});
-          }}
-        onTermSubmit={() => this.searchApi(this.state.term)}/>
-        <Text>{this.state.term}</Text>
-        {this.state.errorMessage ? <Text>{this.state.errorMessage}</Text> : null }
-        <Text>We have found {this.state.results.length} results</Text>
-      </View>
-    );
-  }
-}
+  return (
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => {
+          setTerm( newTerm);
+        }}
+        onTermSubmit={() => searchApi(term)}/>
+      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   background: {
