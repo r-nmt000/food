2305f28208c6b62d3ae9929fa7c24e06c9303c6f
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-06 12:52:55 +0900

    Introduce navigation

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator  } from "@react-navigation/stack";
+import SearchScreen from "./src/screens/SearchScreen";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Search">
+        <Stack.Screen name="Search" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class SearchScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Search Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
